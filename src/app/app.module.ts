7a3f4868bde8841de199633e999677296aa88dad
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-05-09 13:15:46 -0500

    First version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,51 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './public/home/home.component';
+import { NavBarComponent } from './public/nav-bar/nav-bar.component';
+import { ParticipantsComponent } from './participant/participants/participants.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDialogModule} from "@angular/material/dialog";
+import {ReactiveFormsModule} from "@angular/forms";
+import { TableParticipantsComponent } from './participant/table-participants/table-participants.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {HttpClientModule} from "@angular/common/http";
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    ParticipantsComponent,
+    TableParticipantsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/participant/model/Participant.ts
@@ -0,0 +1,9 @@
+export interface Participant {
+  id: string;
+  firstName: number;
+  lastName: number;
+  photoUrl: string;
+  centerId: number;
+  ranking: number;
+  recordTime: string;
+}
